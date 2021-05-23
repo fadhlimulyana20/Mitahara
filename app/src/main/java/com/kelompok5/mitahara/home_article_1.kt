77bb8d2e8312d4e7e4f07cd20d5dbce116f34008
creativelogy <72054980+creativelogy@users.noreply.github.com>
2021-05-23 22:43:20 +0700

    add home article 1

---
+++ app/src/main/java/com/kelompok5/mitahara/home_article_1.kt
@@ -0,0 +1,11 @@
+package com.kelompok5.mitahara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_article_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_article_1)
+    }
+}
